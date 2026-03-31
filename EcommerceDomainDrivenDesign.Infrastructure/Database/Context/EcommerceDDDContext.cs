916092920fd8056bb1915cd56b5c7d1cc63c8f32
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-31 11:18:33 +0700

    Added database infrastructure

---
+++ EcommerceDomainDrivenDesign.Infrastructure/Database/Context/EcommerceDDDContext.cs
@@ -0,0 +1,32 @@
+﻿using EcommerceDomainDrivenDesign.Domain.Core.Messaging;
+using EcommerceDomainDrivenDesign.Domain.Customers;
+using EcommerceDomainDrivenDesign.Domain.Customers.Orders;
+using EcommerceDomainDrivenDesign.Domain.Products;
+using EcommerceDomainDrivenDesign.Infrastructure.Database.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceDomainDrivenDesign.Infrastructure.Database.Context
+{
+    public sealed class EcommerceDDDContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<StoredEvent> StoredEvents { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+
+        public EcommerceDDDContext(DbContextOptions<EcommerceDDDContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Ignore<Event>();
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new StoredMessageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+    }
+}
