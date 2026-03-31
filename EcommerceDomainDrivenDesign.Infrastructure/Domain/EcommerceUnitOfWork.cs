c0568a834bca481697e4a8b1a2497b5cc4a92cf2
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-31 11:25:33 +0700

    Added domain infrastructure implementation

---
+++ EcommerceDomainDrivenDesign.Infrastructure/Domain/EcommerceUnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EcommerceDomainDrivenDesign.Domain;
+using EcommerceDomainDrivenDesign.Domain.Core.Base;
+using EcommerceDomainDrivenDesign.Domain.Core.Messaging;
+using EcommerceDomainDrivenDesign.Domain.Customers;
+using EcommerceDomainDrivenDesign.Domain.Products;
+using EcommerceDomainDrivenDesign.Infrastructure.Database.Context;
+using EcommerceDomainDrivenDesign.Infrastructure.Domain;
+using EcommerceDomainDrivenDesign.Infrastructure.Messaging;
+
+namespace EcommerceDomainDrivenDesign.Infrastructure.Domain
+{
+    public class EcommerceUnitOfWork : UnitOfWork<EcommerceDDDContext>, IEcommerceUnitOfWork
+    {
+        public ICustomerRepository CustomerRepository { get; }
+        public IStoredEventRepository MessageRepository { get; }
+        public IProductRepository ProductRepository { get; }
+
+        private readonly IEventSerializer _eventSerializer;
+
+        public EcommerceUnitOfWork(EcommerceDDDContext dbContext,
+            ICustomerRepository customerRepository,
+            IStoredEventRepository messageRepository,
+            IProductRepository productRepository,
+            IEventSerializer eventSerializer) : base(dbContext)
+        {
+            CustomerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+            MessageRepository = messageRepository ?? throw new ArgumentNullException(nameof(messageRepository));
+            ProductRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+
+            _eventSerializer = eventSerializer ?? throw new ArgumentNullException(nameof(eventSerializer));
+        }
+
+        protected async override Task StoreEvents(CancellationToken cancellationToken)
+        {
+            var entities = DbContext.ChangeTracker.Entries()
+                    .Where(e => e.Entity is Entity c && c.DomainEvents != null)
+                    .Select(e => e.Entity as Entity)
+                    .ToArray();
+
+            foreach (var entity in entities)
+            {
+                var messages = entity.DomainEvents
+                    .Select(e =>
+                    StoredEventHelper.BuildFromDomainEvent(e, _eventSerializer)).ToArray();
+
+                entity.ClearDomainEvents();
+                await DbContext.AddRangeAsync(messages, cancellationToken);
+            }
+        }
+    }
+}
---
+++ EcommerceDomainDrivenDesign.Infrastructure/Domain/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using EcommerceDomainDrivenDesign.Domain.Core.Base;
+
+namespace EcommerceDomainDrivenDesign.Infrastructure.Domain
+{
+    public abstract class UnitOfWork<TDB> : IUnitOfWork
+        where TDB : DbContext
+    {
+        protected readonly TDB DbContext;
+
+        protected UnitOfWork(TDB dbContext)
+        {
+            DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<bool> CommitAsync(CancellationToken cancellationToken)
+        {
+            await StoreEvents(cancellationToken);
+            return await DbContext.SaveChangesAsync(cancellationToken) > 0;
+        }
+
+        protected abstract Task StoreEvents(CancellationToken cancellationToken);
+    }
+}
